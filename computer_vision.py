f4c66fb22745ea6cfa893c726b8a6d23bd5a862d
Hien Quoc <hien.g.quoc@gmail.com>
2022-07-07 15:49:12 -0500

    update

--- computer_vision.py
+++ computer_vision.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from utility.utility import Utility
 import os
+import sys
+import os.path
 
 
 class Computer_vision:
@@ -45,6 +47,7 @@ class Computer_vision:
 
     def read_video(self):
         # Read video
+
         self.video = cv2.VideoCapture(self.video_file_path)
         self.debug.title = 'class: Computer_vision def: read_video'
         self.debug.debug_variable_dictionary = {'video_file_path': self.video_file_path}
@@ -55,11 +58,12 @@ class Computer_vision:
 
     def read_frame(self):
         # Read first frame
-        self.video_status, self.video_frame = self.video.read()
-        self.debug.title = 'class: Computer_vision def: read_frame'
-        self.debug.debug_variable_dictionary = {'video_status': self.video_status,
-                                                'video_frame': self.video_frame}
-        self.debug.print_value_dictionary(self.debug.debug_variable_dictionary)
+        while == True:
+            self.video_status, self.video_frame = self.video.read()
+            self.debug.title = 'class: Computer_vision def: read_frame'
+            self.debug.debug_variable_dictionary = {'video_status': self.video_status,
+                                                    'video_frame': self.video_frame}
+            self.debug.print_value_dictionary()
 
     def object_tracker_main(self):
         # Initialize tracker
@@ -67,4 +71,94 @@ class Computer_vision:
         self.video_file_path = computer_vision_class_file_path + '\soccer-ball.mp4'
         self.setup_tracker()
         self.read_video()
+        self.read_frame()
         # Initialize tracker with first frame and bounding box
+
+    def yolo_object_dectection(self):
+        # Initialize the parameters
+        objectnessThreshold = 0.5  # Objectness threshold
+        confThreshold = 0.5  # Confidence threshold
+        nmsThreshold = 0.4  # Non-maximum suppression threshold
+        inpWidth = 416  # Width of network's input image
+        inpHeight = 416  # Height of network's input image
+
+        # Load names of classes
+        computer_vision_class_file_path = os.path.abspath(os.getcwd())
+        classesFile = computer_vision_class_file_path + "\coco.names"
+        classes = None
+        with open(classesFile, 'rt') as f:
+            classes = f.read().rstrip('\n').split('\n')
+
+        # Give the configuration and weight files for the model and load the network using them.
+        modelConfiguration = computer_vision_class_file_path + "\yolov3.cfg"
+        modelWeights = computer_vision_class_file_path + "\yolov3.weights"
+
+        net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+    # Get the names of the output layers
+    def getOutputsNames(self, net):
+        # Get the names of all the layers in the network
+        layersNames = net.getLayerNames()
+        # Get the names of the output layers, i.e. the layers with unconnected outputs
+        return [layersNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# Draw the predicted bounding box
+    def drawPred(self, classId, conf, left, top, right, bottom):
+        # Draw a bounding box.
+        cv2.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+
+        label = '%.2f' % conf
+
+        # Get the label for the class name and its confidence
+        if classes:
+            assert (classId < len(classes))
+            label = '%s:%s' % (classes[classId], label)
+
+        # Display the label at the top of the bounding box
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        top = max(top, labelSize[1])
+        cv2.rectangle(frame, (left, top - round(1.5 * labelSize[1])), (left + round(1.5 * labelSize[0]), top + baseLine),
+                      (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
+
+    # Remove the bounding boxes with low confidence using non-maxima suppression
+    def postprocess(self, frame, outs):
+        frameHeight = frame.shape[0]
+        frameWidth = frame.shape[1]
+
+        classIds = []
+        confidences = []
+        boxes = []
+        # Scan through all the bounding boxes output from the network and keep only the
+        # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+        classIds = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                if detection[4] > objectnessThreshold :
+                    scores = detection[5:]
+                    classId = np.argmax(scores)
+                    confidence = scores[classId]
+                    if confidence > confThreshold:
+                        center_x = int(detection[0] * frameWidth)
+                        center_y = int(detection[1] * frameHeight)
+                        width = int(detection[2] * frameWidth)
+                        height = int(detection[3] * frameHeight)
+                        left = int(center_x - width / 2)
+                        top = int(center_y - height / 2)
+                        classIds.append(classId)
+                        confidences.append(float(confidence))
+                        boxes.append([left, top, width, height])
+
+        # Perform non maximum suppression to eliminate redundant overlapping boxes with
+        # lower confidences.
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            left = box[0]
+            top = box[1]
+            width = box[2]
+            height = box[3]
+            drawPred(classIds[i], confidences[i], left, top, left + width, top + height)
